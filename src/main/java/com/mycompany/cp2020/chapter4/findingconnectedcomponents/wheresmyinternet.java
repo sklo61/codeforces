79185b0cf2bbb1f76e9c7c13632c7bc7bab60458
Lo, Sin Kuang <sinkuang.lo@data61.csiro.au>
2020-10-13 17:33:42 +1100

    kattis

---
+++ src/main/java/com/mycompany/cp2020/chapter4/findingconnectedcomponents/wheresmyinternet.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cp2020.chapter4.findingconnectedcomponents;
+
+import java.util.*;
+import java.io.*;
+
+public class wheresmyinternet {
+
+    static class FastScanner {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner fs = new FastScanner();
+        out.print("Hello World!");
+        out.close ();
+    }
+
+  
+}
+
+
