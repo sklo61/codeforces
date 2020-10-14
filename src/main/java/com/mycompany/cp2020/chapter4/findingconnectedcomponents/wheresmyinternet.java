feac48c3019b24311095ef16beea5584e7415353
Lo, Sin Kuang <sinkuang.lo@data61.csiro.au>
2020-10-14 16:55:45 +1100

    kattis bfwrite shi ttt

--- src/main/java/com/mycompany/cp2020/chapter4/findingconnectedcomponents/wheresmyinternet.java
+++ src/main/java/com/mycompany/cp2020/chapter4/findingconnectedcomponents/wheresmyinternet.java
@@ -10,6 +10,9 @@ import java.io.*;
 
 public class wheresmyinternet {
 
+    static boolean[] visited;
+    static HashMap<Integer, List<Integer>> ht;
+
     static class FastScanner {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,20 +37,78 @@ public class wheresmyinternet {
         }
     }
 
-    public static void main(String[] args) {
-        PrintWriter out = new PrintWriter(System.out);
-        FastScanner fs = new FastScanner();
-        HashMap<Integer, Integer> ht = new HashMap<>();
-        int nums = fs.nextInt();
-        int lines = fs.nextInt();
-        for(int i = 0; i < lines; i++){
-            int a = fs.nextInt();
-            int b = fs.nextInt();
-            
+    static void dfs(int start) {
+        visited[start] = true;
+        for (int i : ht.get(start)) {
+            if (!visited[i]) {
+                dfs(i);
+            }
         }
     }
 
-  
-}
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner fs = new FastScanner();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ht = new HashMap<>();
+        int nums = fs.nextInt();
+        int lines = fs.nextInt();
+        visited = new boolean[nums + 1];
 
+        for (int i = 0; i < lines; i++) {
+            int a = fs.nextInt();
+            int b = fs.nextInt();
+
+            if (ht.containsKey(a)) {
+                ht.get(a).add(b);
+            } else {
+                ht.put(a, new ArrayList<>());
+                ht.get(a).add(b);
+            }
+
+            if (ht.containsKey(b)) {
+                ht.get(b).add(a);
+            } else {
+                ht.put(b, new ArrayList<>());
+                ht.get(b).add(a);
+            }
+        }
+
+        boolean flag = true;
+
+        /**
+         * if (!ht.containsKey(1)) { for(int i = 2; i <= nums; i++){
+         * System.out.println(i); } }else{ LinkedList<Integer> queue = new
+         * LinkedList<Integer>(); queue.add(1);
+         *
+         * while (!queue.isEmpty()) { int a = queue.pop();
+         *
+         * if (visited[a - 1] == false) { visited[a - 1] = true;
+         *
+         * for (int b : ht.get(a)) { if(visited[b-1] == false){ queue.add(b); }
+         * } } }
+         */
+        if (ht.containsKey(1)) {
+            dfs(1);
+        }
+
+        for (int i = 1; i < nums; i++) {
+            if (visited[i] == false) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag == true) {
+            System.out.println("Connected");
+        } else {
+            for (int i = 2; i < visited.length; i++) {
+                if (!visited[i]) {
+                    bw.append(i + "\n");
+                }
+            }
+            bw.flush();
+        }
+    }
+}
 
